94e757c40e3a6c0b68f716aee3dab7295b9b1f72
syuki.nishida <syuki.nishida@digitalidentity.co.jp>
2015-09-24 15:23:43 +0900

    ウィンドウ置き換え

--- shader_framework_from_directx9/src/main.cpp
+++ shader_framework_from_directx9/src/main.cpp
@@ -11,161 +11,61 @@
 #define _CRTDBG_MAP_ALLOC
 #include <windows.h>
 #include <crtdbg.h>
+#include <WindowModule.h>
+#include <SafeDelete.h>
+#include <FrameKeeper.h>
 #include "application.h"
 
-#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "DirectX9Framework.lib")
 
-#define CLASS_NAME ("class")
 #define WINDOW_WIDTH (800)
 #define WINDOW_HEIGHT (600)
-
-//=============================================================================
-//	プロトタイプ宣言
-//=============================================================================
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-//=============================================================================
-//	グローバル変数
-//=============================================================================
 
 //-----------------------------------------------------------------------------
 //	メイン関数
 //-----------------------------------------------------------------------------
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 
-	UNREFERENCED_PARAMETER(hPrevInstance);	// 無くても良いけど、警告が出る（未使用宣言）
-	UNREFERENCED_PARAMETER(lpCmdLine);		// 無くても良いけど、警告が出る（未使用宣言）
+  // ウィンドウ初期化
+  snlib::WindowModule* window_ = new snlib::WindowModule(L"shader framework", WINDOW_WIDTH, WINDOW_HEIGHT, hInstance);
+  window_->Initialize();
+  window_->SetMessageCallback(WM_DESTROY, [](HWND, WPARAM, LPARAM) {
+    PostQuitMessage(0);
+  });
+  window_->SetMessageCallback(WM_KEYDOWN, [](HWND h, WPARAM w, LPARAM) {
+    if (w == VK_ESCAPE) { DestroyWindow(h); }
+  });
 
-	DWORD dwExecLastTime;					//FPS表示用変数
-	DWORD dwFPSLastTime;
-	DWORD dwCurrentTime;
-	DWORD dwFrameCount;
+  // アプリケーション初期化
+  Application* app = new Application;
+  app->Initialize(window_->GetWindowHandle());
 
-	//ウィンドウクラス生成
-	WNDCLASSEX	wcex = {
-		sizeof(WNDCLASSEX),
-		CS_CLASSDC,
-		WndProc,
-		0,
-		0,
-		hInstance,
-		NULL,
-		LoadCursor(NULL, IDC_ARROW),
-		(HBRUSH)(COLOR_WINDOW + 1),
-		NULL,
-		CLASS_NAME,
-		NULL
-	};
-	HWND hWnd;								//ウィンドウハンドル
-	MSG msg;
+  // フレームキーパー初期化
+  auto update_func = [app] {
+    // アプリケーションの更新と描画
+    app->Update();
+    app->Draw();
+  };
+  snlib::FrameKeeper* frame_keeper_ = new snlib::FrameKeeper(60, update_func);
 
-	// ウィンドウクラスの登録
-	RegisterClassEx(&wcex);
-
-	// ウィンドウの作成
-	hWnd = CreateWindowEx(0,
-						CLASS_NAME,
-						CLASS_NAME,
-						WS_OVERLAPPEDWINDOW,//WS_OVERLAPPEDWINDOW |WS_VSCROLL / WS_MINIMIZEBOX / WS_OVERLAPPEDWINDOW & ~(WS_THICKFRAME |WS_MAXIMIZEBOX)
-						CW_USEDEFAULT,
-						CW_USEDEFAULT,
-						((int)WINDOW_WIDTH + GetSystemMetrics(SM_CXDLGFRAME) * 2),
-						((int)WINDOW_HEIGHT + GetSystemMetrics(SM_CXDLGFRAME) * 2 + GetSystemMetrics(SM_CYCAPTION)),
-						NULL,
-						NULL,
-						hInstance,
-						NULL);
-
-	//フレームカウント初期化
-	timeBeginPeriod(1);				// 分解能を設定
-	dwExecLastTime = 
-	dwFPSLastTime = timeGetTime();
-	dwCurrentTime =
-	dwFrameCount = 0;
-
-	// ウインドウの表示(初期化処理の後に行う)
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-
-	// アプリケーション初期化
-	Application app;
-	app.Initialize(hWnd);
-
-	// メッセージループ
-	while(1)
-	{
-		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) != 0)	// メッセージを取得しなかった場合"0"を返す
-		{// Windowsの処理
-			if(msg.message == WM_QUIT)
-			{// PostQuitMessage()が呼ばれたらループ終了
-				break;
-			}
-			else
-			{
-				// メッセージの翻訳と送出
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
-		}
-		else
-		{
-			dwCurrentTime = timeGetTime();
-			if((dwCurrentTime - dwFPSLastTime) >= 500)	// 0.5秒ごとに実行
-			{
-				dwFPSLastTime = dwCurrentTime;
-				dwFrameCount = 0;
-			}
-
-			if((dwCurrentTime - dwExecLastTime) >= (1000 / 60))
-			{
-				dwExecLastTime = dwCurrentTime;
-
-				// アプリケーションの更新と描画
-				app.Update();
-				app.Draw();
-
-				dwFrameCount++;
-			}
-		}
-	}
+  // メインループ
+  while (window_->MessageProcessing()) {
+    frame_keeper_->Step();
+  }
 
 	// アプリケーションの終了
-	app.Finalize();
+	app->Finalize();
+  snlib::SafeDelete(app);
 	
-	// ウィンドウクラスの登録を解除
-	UnregisterClass(CLASS_NAME, wcex.hInstance);
+  // フレームキーパー終了
+  snlib::SafeDelete(frame_keeper_);
 
-	timeEndPeriod(1);				// 分解能を戻す
+  // ウィンドウ終了
+  window_->Finalize();
+  snlib::SafeDelete(window_);
 
-	return (int)msg.wParam;
+	return 0;
 }
 
-//=============================================================================
-// プロシージャ
-//=============================================================================
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch(uMsg)
-	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-	case WM_KEYDOWN:
-		switch(wParam)
-		{
-		case VK_ESCAPE:			//Escが押されたらウィンドウを破棄して終了
-			DestroyWindow(hWnd);
-			break;
-		}
-		break;
-
-	default:
-		break;
-	}
-
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
-
+// EOF
