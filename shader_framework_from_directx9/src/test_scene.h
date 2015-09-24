b19117e2eb43b8701e241b1aaad43a3d754e31e9
syuki.nishida <syuki.nishida@digitalidentity.co.jp>
2015-09-24 16:20:27 +0900

    実験場をシーンに切り離しわかりやすく

---
+++ shader_framework_from_directx9/src/test_scene.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <d3d9.h>
+#include "perth_matrix.h"
+#include "view_matrix.h"
+#include "vertex_shader.h"
+#include "pixel_shader.h"
+#include "plane.h"
+#include "xmodel.h"
+
+class TestScene {
+public:
+  /// @brief コンストラクタ
+  TestScene();
+
+  /// @brief デストラクタ
+  virtual ~TestScene();
+
+  /// @brief 初期化
+  void Initialize(LPDIRECT3DDEVICE9 device);
+
+  /// @brief 終了
+  void Finalize();
+
+  /// @brief 更新
+  void Update();
+
+  /// @brief 描画
+  void Draw(LPDIRECT3DDEVICE9 device);
+
+protected:
+private:
+  VertexShader* vs_;
+  PixelShader* ps_;
+
+  PerthMatrix perth_;
+  ViewMatrix view_;
+
+  Plane* plane_;
+  XModel* model_;
+};
